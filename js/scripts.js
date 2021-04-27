92a318b5d0b3c1517d0477b06edc238cb3069c87
David Miller <hello@davidmiller.io>
2021-04-27 15:52:46 -0400

    Bootstrap 4.6.0 update and dev environment change

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Blog Post v4.3.0 (https://startbootstrap.com/template/blog-post)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-post/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
